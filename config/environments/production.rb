7283d9de9c2ed6a2fe2d12079729da17aa48fe9c
Čeněk Drbal <cendrb@gmail.com>
2016-06-08 16:36:16 +0200

    action cable config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = 'ws://kanathegame.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ["https://kanathegame.herokuapp.com", "http://kanathegame.herokuapp.com"]
 end
